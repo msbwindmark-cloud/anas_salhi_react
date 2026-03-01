376dfcdb931734ff681f28275a7605eeae48eeb1
mohammed sebti <mohammed.sebti@agqlabs.com>
2026-03-01 16:35:49 +0100

    Configurada ruta principal para React

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')), # Conectamos nuestra app
+    # 3. CUALQUIER otra ruta la maneja React (Esto va al final)
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 # Esto permite ver las fotos/archivos subidos durante el desarrollo
