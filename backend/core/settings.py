a3b010a6977aa3cfeeb066b5a3bf0d66f430d2ad
mohammed sebti <mohammed.sebti@agqlabs.com>
2026-03-01 16:38:10 +0100

    Configurado Django para servir React en producción

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / 'frontend' / 'dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
